cc37331e21513f8ba2abb77ce299b15f9a6ba783
Lucie <lucie.valls@yahoo.fr>
2012-12-17 11:27:46 +0100

    Joueur
    Classse Joueur ajoutée

---
+++ Projet_echec/src/main/java/foo/Joueur.java
@@ -0,0 +1,25 @@
+package foo;
+
+public class Joueur {
+	
+	// Attributs de la classe
+	
+	private String Nom;
+	private String Couleur;
+	private int Score;
+	
+	
+	// Méthodes de la classe 
+	
+	public Joueur() // Constructeur de la classe Joueur
+	{ 
+		
+	}
+	
+	public String AjouterJoueur(String Nom, String Couleur) // Méthode pour ajouter un Joueur avec son nom et sa couleur
+	
+	{
+		
+	}
+
+}
