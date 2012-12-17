95e973bc4b083528f13d755c52953b17b842e823
CyrilMazouyer <cyril.mazouyer@live.fr>
2012-12-17 14:08:51 +0100

    echiquier
    echiquier

---
+++ Projet_echec/src/main/java/foo/Echiquier.java
@@ -0,0 +1,41 @@
+import java.util.Vector;
+
+/* Cyril Mazouyer */
+/* 17 Décembre 2012 */
+/* Classe Echiquier */
+
+public class Echiquier
+{
+	/* Déclaration des attributs de la classe */
+	private Piece tableau[][];
+	private boolean aide;
+	
+	/* Déclaration du constructeur de la classe */
+	public Echiquier()
+	{
+		
+	}
+	
+	/* Déclaration des méthodes de la classe */
+	public Echiquier echiquierStandard()
+	{
+		
+	}
+	
+	public boolean deplacerPiece(Piece piece, String destination)
+	{
+	
+	}
+	
+	public boolean ajouterPiece(String nom, String position)
+	{
+
+	}
+	
+	public boolean supprimerPiece(String position)
+	{
+		
+	}
+	
+	public Vector<> destinationPossible
+}
