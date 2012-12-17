4fe18a66dd6b59eeb038712ac2c1d5541b730202
Damien Eon <eon.damien@hotmail.fr>
2012-12-17 18:11:00 +0100

    Revert "coucou2"
    This reverts commit 142283999b94232f8fc86170c5fccf7e94a4e831.

---
+++ Projet_echec/src/main/java/foo/Affichage.java
@@ -0,0 +1,32 @@
+package foo;
+
+public class Affichage {
+	//Interface Affichage
+	public void menuPrincipal(){
+		
+	}
+	
+	public void optionPartie(){
+		
+	}
+	
+	public void optionFinDePartie(){
+		
+	}
+	
+	public void choixDeLaPartie(){
+		
+	}
+	
+	public void confirmExit(){
+		
+	}
+	
+	public void afficherEchiquier(){
+		
+	}
+	
+	public void afficherPosition(){
+		
+	}
+}
