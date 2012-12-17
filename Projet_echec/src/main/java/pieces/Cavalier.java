da8d31f4d9dadd668970ef42d8bc1380095f2091
FesseL <mini.dindon@gmail.com>
2012-12-17 22:59:50 +0100

    positionpossible cavalier
    position du cavalier avec dépassement de l'échiquier

--- Projet_echec/src/main/java/pieces/Cavalier.java
+++ Projet_echec/src/main/java/pieces/Cavalier.java
@@ -2,75 +2,78 @@ package pieces;
 
 public class Cavalier extends Piece{
 
-	public Cavalier(Position position,String couleur) {
+	public Cavalier(Position position, String couleur){
 		super(position,couleur);
 		this.setPositionPossible();
 		this.Image="image Cavalier de cette couleur";
 	}
 
-	public boolean setPositionPossible() {
+	public boolean setPositionPossible(){
 		int i = this.position.getI();
 		int j = this.position.getJ();
 
-		if (i<)
-		for (int a =0 ; a<8 ; a++){
-			PositionPossible[a][j]=1;
+		for(int a=0; a<8; a++){
+			for(int b=0; b<8; b++){
+				PositionPossible[a][b]=0;
+			}
 		}
 		
-		for (int a =0 ; a<8 ; a++){
-			PositionPossible[i][a]=1;
-		}
-		
-		while (i<8 && j<8){
+		//position actuelle du cavalier
+		PositionPossible[i][j]=0;
+		//position en haut à droite
+		i=i-2;
+		j++;
+		if(i>=0 && j>=0 && i<=7 && j<=7){//contrainte de dépassement de l'échiquier
 			PositionPossible[i][j]=1;
-			i++;
-			j++;
 		}
-		
-		 i = this.position.getI();
-		 j = this.position.getJ();
-		 
-		 
-		while (i>=0 && j<8){
+		//position vers la droite en haut
+		i++;
+		j++;
+		if(i>=0 && j>=0 && i<=7 && j<=7){
 			PositionPossible[i][j]=1;
-			i--;
-			j++;
 		}
-		
-		 i = this.position.getI();
-		 j = this.position.getJ();
-		 
-		while (i<8 && j>=0){
+		//position vers la droite en bas
+		i=i+2;
+		if(i>=0 && j>=0 && i<=7 && j<=7){
 			PositionPossible[i][j]=1;
-			i++;
-			j--;
 		}
-		
-		 i = this.position.getI();
-		 j = this.position.getJ();
-		
-		while (i>=0 && j>=0){
+		//position vers le bas à droite
+		i++;
+		j--;
+		if(i>=0 && j>=0 && i<=7 && j<=7){
 			PositionPossible[i][j]=1;
-			i--;
-			j--;
+		}
+		//position vers le bas à gauche
+		j=j-2;
+		if(i>=0 && j>=0 && i<=7 && j<=7){
+			PositionPossible[i][j]=1;
+		}
+		//position vers la gauche en bas
+		i--;
+		j--;
+		if(i>=0 && j>=0 && i<=7 && j<=7){
+			PositionPossible[i][j]=1;
+		}
+		//position vers la gauche en haut
+		i=i-2;
+		if(i>=0 && j>=0 && i<=7 && j<=7){
+			PositionPossible[i][j]=1;
+		}
+		//position vers le haut à droite
+		i--;
+		j++;
+		if(i>=0 && j>=0 && i<=7 && j<=7){
+			PositionPossible[i][j]=1;
 		}
 		
-		i = this.position.getI();
-		j = this.position.getJ();
-		 
-	PositionPossible[i][j]=0;		
-			
-	return true;		
-	
+		return true;
 	}
 	
-	
-	
-
-	
-	public void toStringCavalier() {//affiche les caracteristique du cavalier
-		System.out.println("Cavalier [position=" + position.getI() + position.getJ() + ", Couleur=" + Couleur + "]"+"PositionPossible= " );
-		this.toStringPositionPossible();
+	public void toStringCavalier() {//affiche les caracteristiques du cavalier
+		this.setPosition(position);
+		System.out.println("Cavalier [position=" + position.getI()+ " " + position.getJ() + ", Couleur=" + Couleur
+				+ ", Image=" + Image + ", PositionPossible=");
+				 this.toStringPositionPossible();
 	}
 	
 }
